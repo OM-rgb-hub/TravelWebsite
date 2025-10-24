4db12a23c4cbd21a8d4a4f33815a7fa0bfafc7c1
OM JADHAV <ojjadhav.007@gmail.com>
2025-10-24 16:51:18 +0530

    Add Projects Files

---
+++ routes/listing.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const { storage } = require("../cloudConfig.js");
+const multer = require("multer");
+const upload = multer({ storage });
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image][url]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+//Show Route
+router.get("/:id", wrapAsync(listingController.showListing));
+
+//Create Route
+
+//Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+//Update Route
+router.put(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  upload.single("listing[image][url]"),
+  validateListing,
+  wrapAsync(listingController.updateListing)
+);
+
+//Delete Route
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.destroyListing)
+);
+
+module.exports = router;
---
+++ schema.js
@@ -0,0 +1,42 @@
+// const Joi = require('joi');
+
+// module.exports.listingSchema = Joi.object({
+//     listing : Joi.object({
+//         title: Joi.string().required(),
+//         description: Joi.string().required(),
+//         location: Joi.string().required(),
+//         country: Joi.string().required(),
+//         price: Joi.number().required().min(0),
+//         image: Joi.string().allow(" ",null)
+//     }).required()
+// })
+
+// module.exports.reviewSchema = Joi.object({
+//     review: Joi.object({
+//         rating: Joi.number().required().min(1).max(5),
+//         comment: Joi.string().required(),
+//     }).required(),
+// });
+
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.object({          // <-- make image an object
+      url: Joi.string().allow("", null),
+      filename: Joi.string().allow("", null)
+    }).optional()                // <-- optional for edit
+  }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
+  }).required(),
+});
